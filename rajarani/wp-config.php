36e08dac4a3a65d1659022b608b2b622f3f73365
jay-borsaniya <jayborsaniya.softrefine@gmail.com>
2023-04-20 12:04:42 +0530

    change in config file

--- rajarani/wp-config.php
+++ rajarani/wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'rajarani' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'wordpress' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'password' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
